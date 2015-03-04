31fac85c592b5991a13dd982ce2e28e503ce11a5
GreenOn <pramo.dvadrevu@gmail.com>
2015-03-04 15:31:37 -0600

    Pushing initial commit.

---
+++ OTSMembers/Models/MemberSponsorship.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OTSMembers.Models
+{
+    public class MemberSponsorship
+    {
+        public enum ModeOfPayment {
+            Check,
+            Online,
+            CreditCard,
+            Cash
+        };
+        public int Id { get; set; }
+        public DateTime PaymentDate{ get; set; }
+        public decimal Amount { get; set; }
+        public ModeOfPayment TypeOfPayment  { get; set; }
+        public string ReferredBy { get; set; }
+        public bool Anonymous { get; set; }
+    }
+}
---
+++ OTSMembers/Models/OTSDb.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace OTSMembers.Models
+{
+    public class OtsDb : DbContext
+    {
+        public OtsDb() : base("name=DefaultConnection")
+        {
+
+        }
+        public DbSet<OtsMember> OTSMembers{ get; set; }
+        public DbSet<MemberSponsorship> Sponsorships{ get; set; }
+    }
+}
