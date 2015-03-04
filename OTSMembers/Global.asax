6657cc787ed1b3073a3f97f2398cd07cb8d2347a
GreenOs <pramodvadrevu@gmail.com>
2015-03-04 14:53:33 -0600

    Revert "Revert "Initial Commit""
    This reverts commit 1f6b03386764df659c0465f09dc396b2ea94fecd.

---
+++ OTSMembers/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OTSMembers.MvcApplication" Language="C#" %>
---
+++ OTSMembers/Models/OtsMember.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OTSMembers.Models
+{
+    public class OtsMember
+    {
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string SpouseName { get; set; }
+        public string StreeAddress { get;   set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+        public string Notes { get; set; }
+        public bool OkToPublish { get; set; }
+        public ICollection<MemberSponsorship> Sponsorship { get; set; }
+    }
+}
